cfefae612a3d55f05b94fc560c1118e67960ebf9
Gottfried Prasetyadi <gottfriedcpn@gmail.com>
2019-03-24 19:50:46 +0700

    Ann model on Admin site

--- homepage/admin.py
+++ homepage/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Announcement
+
+class AnnouncementAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date_created', 'date_expired', 'summary')
 
 # Register your models here.
+admin.site.register(Announcement, AnnouncementAdmin)
